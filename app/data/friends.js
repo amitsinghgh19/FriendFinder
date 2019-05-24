4118906d9340d60517e1175e2c76c27c4e6bba73
amitsinghgh19 <prasadmanigh19>
2019-05-24 12:34:18 -0400

    fixed friends.js and added celebrities pictures

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+// Friends JSON file
+var friends = [
+	{
+	name: "Ranveer Singh",
+	photo: "RanveerSingh.jpg",
+	scores: [
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
+			]
+	},
+	{
+	name: "Aamir Khan",
+	photo: "AamirKhan.jpg",
+	scores: [
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4"
+			]
+	},
+	{
+	name: "Hrithik Roshan",
+	photo: "HrithikRoshan.jpg",
+	scores: [
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3"
+			]
+	},
+	{
+	name: "Deepika Padukone",
+	photo: "DeepikaPadukone.jpg",
+	scores: [
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2",
+			"2"
+			]
+	},
+	{
+	name: "Priyanka Chopra",
+	photo: "PriyankaChopra.jpg",
+	scores: [
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1"
+			]
+	}
+];
+
+module.exports = friends;
